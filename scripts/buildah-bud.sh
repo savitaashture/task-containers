c9ef1140231865c485c8d910d5af6d22054278d3
anandxkumar <anandkumar26sep00@gmail.com>
2023-05-16 14:13:55 +0530

    Create buildah-common.sh file, made more structured
    Signed-Off-By: Anand Kumar <anakumar@redhat.com>

--- scripts/buildah-bud.sh
+++ scripts/buildah-bud.sh
@@ -4,67 +4,14 @@ shopt -s inherit_errexit
 set -eu -o pipefail
 
 source "$(dirname ${BASH_SOURCE[0]})/common.sh"
-
-phase "Declaring Environment Variables"
-
-declare -rx PARAMS_IMAGE="${PARAMS_IMAGE:-}"
-declare -rx PARAMS_STORAGE_DRIVER="${PARAMS_STORAGE_DRIVER:-}"
-declare -rx PARAMS_CONTAINERFILE_PATH="${PARAMS_CONTAINERFILE_PATH:-}"
-declare -rx PARAMS_CONTEXT_SUBDIRECTORY="${PARAMS_CONTEXT_SUBDIRECTORY:-}"
-declare -rx PARAMS_TLS_VERIFY="${PARAMS_TLS_VERIFY:-}"
-declare -rx PARAMS_BUILD_EXTRA_ARGS="${PARAMS_BUILD_EXTRA_ARGS:-}"
-declare -rx PARAMS_PUSH_EXTRA_ARGS="${PARAMS_PUSH_EXTRA_ARGS:-}"
-declare -rx PARAMS_SKIP_PUSH="${PARAMS_SKIP_PUSH:-}"
-
-declare -rx WORKSPACES_SOURCE_PATH="${WORKSPACES_SOURCE_PATH:-}"
-declare -rx WORKSPACES_SOURCE_BOUND="${WORKSPACES_SOURCE_BOUND:-}"
-
-declare -rx RESULTS_IMAGE_DIGEST="${RESULTS_IMAGE_DIGEST:-}"
-declare -rx RESULTS_IMAGE_URL="${RESULTS_IMAGE_URL:-}"
-
-#
-# Additional Configuration
-#
-
-# full path to the target application source code directory
-# declare -rx SOURCE_DIR="${WORKSPACES_SOURCE_PATH}/${PARAMS_CONTEXT_SUBDIRECTORY}"
-
-declare -rx TEKTON_HOME="${TEKTON_HOME:-/tekton/home}"
-
-
-#
-# Asserting Environment
-#
-
-
-declare -ra required_vars=(
-    WORKSPACES_SOURCE_PATH
-    PARAMS_IMAGE
-    PARAMS_CONTAINERFILE_PATH
-)
-
-for v in "${required_vars[@]}"; do
-    [[ -z "${!v}" ]] &&
-        fail "'${v}' environment variable is not set!"
-done
-
-phase "Preparing the Workspaces, setting the expected ownership and permissions"
-chmod -v 775 "${WORKSPACES_SOURCE_PATH}"
-
-if [[ "${WORKSPACES_SOURCE_BOUND}" == "true" ]]; then
-    echo "WORKSPACE EXISTS"
-    # Define the Dockerfile contents
-    echo "FROM alpine:3.14" >> "/Dockerfile"
-    echo "CMD echo 'Hello world" >> "/Dockerfile"
-    cat $(/Dockerfile)
-else
-    fail "WORKSPACE_SOURCE is not bounded."
-fi
-
+source "$(dirname ${BASH_SOURCE[0]})/buildah-common.sh"
 
 phase "Executing buildah bud script"
 
+set -x
 exec buildah --storage-driver=${PARAMS_STORAGE_DRIVER} bud \
         ${PARAMS_BUILD_EXTRA_ARGS}  \
         --tls-verify=${PARAMS_TLS_VERIFY} --no-cache \
-        -f ${PARAMS_CONTAINERFILE_PATH} -t ${PARAMS_IMAGE} ${PARAMS_CONTEXT_SUBDIRECTORY}
+        -f ${PARAMS_CONTAINERFILE_PATH} \
+        -t ${PARAMS_IMAGE} \
+        ${PARAMS_CONTEXT_SUBDIRECTORY}

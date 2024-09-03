e5b82ff9db8eae69bad1d1106deb59b1eefe353d
Aneesh-M-Bhat <aneeshbhat3719@gmail.com>
2024-09-03 19:19:52 +0530

    Fixed issue with False for skopeo-copy

--- scripts/skopeo-copy.sh
+++ scripts/skopeo-copy.sh
@@ -14,7 +14,7 @@ phase "Copying '${PARAMS_SOURCE_IMAGE_URL}' into '${PARAMS_DESTINATION_IMAGE_URL
 set -x
 
 if [ -n "${PARAMS_SOURCE_IMAGE_URL}" ] && [ -n "${PARAMS_DESTINATION_IMAGE_URL}" ]; then
-    skopeo copy "${SKOPEO_DEBUG_FLAG}" \
+    skopeo copy ${SKOPEO_DEBUG_FLAG} \
         --src-tls-verify="${PARAMS_SRC_TLS_VERIFY}" \
         --dest-tls-verify="${PARAMS_DEST_TLS_VERIFY}" \
         "${PARAMS_SOURCE_IMAGE_URL}" \
